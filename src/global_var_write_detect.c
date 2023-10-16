5c391bcaf68a9090c85e465a14a7f20b65ae51d0
thegalang <galangkangingotera@hotmail.com>
2023-10-16 20:43:30 +0800

    remove commented code

--- src/global_var_write_detect.c
+++ src/global_var_write_detect.c
@@ -17,14 +17,6 @@ void *mutex;
 
 static void instrument_mem(void *drcontext, instrlist_t *ilist, instr_t *where, opnd_t ref, bool write)
 {
-	/* reserve to registers for our memory address*/
-	reg_id_t reg_ptr, reg_tmp;
-	if (drreg_reserve_register(drcontext, ilist, where, NULL, &reg_ptr) != DRREG_SUCCESS  || 
-		drreg_reserve_register(drcontext, ilist, where, NULL, &reg_tmp) != DRREG_SUCCESS) {
-		DR_ASSERT(false); /* cannot recover */
-		return;
-	}
-
 
 	int opcode = instr_get_opcode(where);
 	opnd_t src = instr_get_src(where, 0);
@@ -36,26 +28,6 @@ static void instrument_mem(void *drcontext, instrlist_t *ilist, instr_t *where, 
 		dr_printf("memory write (opcode %d) from %d to %d\n", opcode, src_new_value, dst_address);
 	}
 
-	// uint32_t size = drutil_opnd_mem_size_in_bytes(ref, where);
-	// drutil_insert_get_mem_addr(drcontext, ilist, where, ref, reg_tmp, reg_ptr);
-
-	// drx_buf_insert_load_buf_ptr(drcontext, memfile_buf, ilist, where, reg_ptr);
-
-	// // writes the memory address (stored in reg_tmp)
-	// drx_buf_insert_buf_store(drcontext, memfile_buf, ilist, where, reg_ptr, DR_REG_NULL, opnd_create_reg(reg_tmp), OPSZ_PTR, offsetof(memfile_t, addr)); 
-	
-	// // writes the memory value
-	// drx_buf_insert_buf_store(drcontext, memfile_buf, ilist, where, reg_ptr, reg_tmp, OPND_CREATE_INT64(0), OPSZ_8, offsetof(memfile_t, value));
-
-	// // writes the size
-	// drx_buf_insert_buf_store(drcontext, memfile_buf, ilist, where, reg_ptr, reg_tmp, OPND_CREATE_INT32(size), OPSZ_4, offsetof(memfile_t, size));
-	
-	// // is it read or write? determined from per_instr
-	// drx_buf_insert_buf_store(drcontext, memfile_buf, ilist, where, reg_ptr, reg_tmp, OPND_CREATE_INT32(write?1:0), OPSZ_4, offsetof(memfile_t, status));
-
-	if (drreg_unreserve_register(drcontext, ilist, where, reg_ptr) != DRREG_SUCCESS ||
-	    drreg_unreserve_register(drcontext, ilist, where, reg_tmp) != DRREG_SUCCESS)
-		DR_ASSERT(false);
 }
 
 static dr_emit_flags_t per_insn_instrument(void *drcontext, void *tag, instrlist_t *bb, instr_t *instr, 
