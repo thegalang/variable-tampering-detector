51a10777b25f0f9220d0fd5b62e9d666cc9a9887
thegalang <galangkangingotera@hotmail.com>
2023-10-17 23:47:29 +0800

    add some code to get registers used for global vars

--- src/global_var_write_detect.c
+++ src/global_var_write_detect.c
@@ -12,20 +12,69 @@
 	#define DISPLAY_STRING(msg) dr_printf("%s\n", msg)
 #endif
 
+
 void *mutex;
 
+void *global_var_address;
+
+/*
+goal: Before memory write, check if written address is same as global
+if same, store written value in buffer
+*/
 static void instrument_mem_write(void *drcontext, instrlist_t *ilist, instr_t *where)
 {
 
+	reg_id_t reg_ptr, reg_tmp, reg_savedval;
 
-	int opcode = instr_get_opcode(where);
+	if (drreg_reserve_register(drcontext, ilist, where, NULL, &reg_tmp) !=
+        DRREG_SUCCESS) {
+        DR_ASSERT(false);
+        return DR_REG_NULL;
+    }
 
+    if (drreg_reserve_register(drcontext, ilist, where, NULL, &reg_ptr) !=
+        DRREG_SUCCESS) {
+        DR_ASSERT(false);
+        return DR_REG_NULL;
+    }
+	
 	opnd_t src = instr_get_src(where, 0);
 	opnd_t dst = instr_get_dst(where, 0);
 
-	ptr_int_t new_value = opnd_get_immed_int(src);
-	void* dst_address = opnd_get_addr(dst);
-	dr_printf("memory write (opcode %d) value %ld to %0x\n", opcode, new_value, dst_address);
+	uint32_t size = drutil_opnd_mem_size_in_bytes(ref, where);
+	drutil_insert_get_mem_addr(drcontext, ilist, where, dst, reg_tmp, reg_ptr);
+
+	// compare if reg_tmp address is same as global address
+	// TODO
+
+	// if its a simple constant load
+	// reg tmp value is no longer needed. can use to scratch
+	if(opnd_is_immed(src)) {
+
+		
+		drx_buf_insert_load_buf_ptr(drcontext, memfile_buf, ilist, where, reg_ptr);
+
+		// todo: store immed value in buffer
+
+	} else if(opnd_is_reg(src)) {
+
+		drx_buf_insert_load_buf_ptr(drcontext, memfile_buf, ilist, where, reg_ptr);
+
+		// todo: read reg value of src and store in reg_savedval
+		// src is a reg, get it and store value in buffer
+		reg_id_t = opnd_get_reg(src);
+
+		// todo: insert immed value in buffer
+		drx_buf_insert_buf_store(drcontext, memfile_buf, ilist, where, reg_ptr, DR_REG_NULL, opnd_create_reg(reg_tmp), OPSZ_PTR, offsetof(memfile_t, addr)); 
+		drx_buf_insert_buf_store(drcontext, memfile_buf, ilist, where, reg_ptr, reg_tmp, OPND_CREATE_INT64(0), OPSZ_8, offsetof(memfile_t, value));
+		drx_buf_insert_buf_store(drcontext, memfile_buf, ilist, where, reg_ptr, reg_tmp, OPND_CREATE_INT32(size), OPSZ_4, offsetof(memfile_t, size));
+	}
+	
+
+
+	// ptr_int_t new_value = opnd_get_immed_int(src);
+	// void* dst_address = opnd_get_addr(dst);
+	// dr_printf("memory write (opcode %d) value %ld to %0x\n", opcode, new_value, dst_address);
 
 }
 
