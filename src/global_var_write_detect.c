190549c47ae949bfe89d1208bb6747768d37a1d4
thegalang <galangkangingotera@hotmail.com>
2023-10-27 02:11:13 +0800

    continue working on comparison

--- src/global_var_write_detect.c
+++ src/global_var_write_detect.c
@@ -75,34 +75,35 @@ static dr_emit_flags_t after_memory_write(void *drcontext, instrlist_t *ilist, i
     opnd_t tst = opnd_create_abs_addr(global_var_address, OPSZ_PTR);
 
     instr_t* loadCurrentGlobalVarValue = INSTR_CREATE_mov_ld(drcontext, opnd_create_reg(reg_tmp), opnd_create_abs_addr(global_var_address, OPSZ_PTR));
-    opnd_t src = instr_get_src(loadCurrentGlobalVarValue, 0);
-    opnd_t dst = instr_get_dst(loadCurrentGlobalVarValue, 0);
+    instr_t* loadSavedGlobalVarValue = INSTR_CREATE_mov_ld(drcontext, opnd_create_reg(reg_ptr), opnd_create_abs_addr(&saved_global_var_value, OPSZ_PTR));
 
     // compare current value of global address with current value of global variable
-    //instr_t* isGlobalVarChange = INSTR_CREATE_cmp(drcontext, opnd_create_abs_addr(&saved_global_var_value, OPSZ_8), opnd_create_reg(reg_tmp));
-    //dr_printf("global_address hello: %p %d %d\n", opnd_get_addr(src), opnd_get_reg(dst), reg_tmp);
+    instr_t* isGlobalVarChange = INSTR_CREATE_cmp(drcontext, opnd_create_reg(reg_tmp), opnd_create_immed_int(12345, OPSZ_8));
+    //opnd_t src = instr_get_src(isGlobalVarChange, 0);
+    //opnd_t dst = instr_get_src(isGlobalVarChange, 1);
+    //dr_printf("global_address hello: %d %d %p %x\n", opnd_get_reg(src), opnd_get_reg(dst), &saved_global_var_value, OP_cmp);
     //dr_printf("saved address: %p\n", &saved_global_var_value);
     
     // if they are different, update occured
-    //instr_t* updateSavedGlobalValue = INSTR_CREATE_movd(drcontext, opnd_create_abs_addr(&saved_global_var_value, OPSZ_PTR), opnd_create_reg(reg_tmp));
-    //instr_t* labelContinue = INSTR_CREATE_label(drcontext);
+    instr_t* updateSavedGlobalValue = INSTR_CREATE_mov_st(drcontext, opnd_create_abs_addr(&saved_global_var_value, OPSZ_PTR), opnd_create_reg(reg_tmp));
+    instr_t* labelContinue = INSTR_CREATE_label(drcontext);
     //instr_t* continueIfEqual = INSTR_CREATE_jcc(drcontext, OP_je, opnd_create_instr(labelContinue));
+    //instr_t* continueIfEqual = INSTR_CREATE_jmp(drcontext, opnd_create_instr(labelContinue));
 
     // insert loadCurrentGlobalVarValue
     instrlist_preinsert(ilist, where, loadCurrentGlobalVarValue);
-    dr_printf("keluar pak eko\n");
+    instrlist_preinsert(ilist, where, loadSavedGlobalVarValue);
     //instrlist_preinsert(ilist, where, isGlobalVarChange);
     
-    //instrlist_preinsert(ilist, where, continueIfEqual);
+    instrlist_preinsert(ilist, where, continueIfEqual);
 
     // code to run if global variable value has changed
-    //instrlist_preinsert(ilist, where, updateSavedGlobalValue);
+    instrlist_preinsert(ilist, where, updateSavedGlobalValue);
 	drx_buf_insert_load_buf_ptr(drcontext, global_history_buf, ilist, where, reg_ptr);
 	drx_buf_insert_buf_store(drcontext, global_history_buf, ilist, where, reg_ptr, DR_REG_NULL, opnd_create_reg(reg_tmp), OPSZ_8, 0);
 	drx_buf_insert_update_buf_ptr(drcontext, global_history_buf, ilist, where, reg_ptr, reg_tmp, OPSZ_8);
 	
-	dr_printf("done all instruction\n");
-	//instrlist_preinsert(ilist, where, labelContinue);
+	instrlist_preinsert(ilist, where, labelContinue);
 	// code to run after global variable change logic is done
 
 
@@ -131,8 +132,6 @@ static dr_emit_flags_t per_insn_instrument(void *drcontext, void *tag, instrlist
 		opnd_is_memory_reference(instr_get_dst(instr, 0))) {
 
 		instr_t* nextInstr = instr_get_next(instr);
-		// debug
-		if(opnd_get_addr(instr_get_dst(instr, 0)) == global_var_address)
 		return after_memory_write(drcontext, bb, nextInstr);
 	} 
 
